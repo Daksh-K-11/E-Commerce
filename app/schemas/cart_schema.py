fdfadf9daefadc77faa9d5ed407220bf5f75c6bc
Daksh-K-11 <khinvasara.daksh@gmail.com>
2025-02-18 14:02:41 +0530

    add cart order schema

---
+++ app/schemas/cart_schema.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import List
+from .product_schema import ProductOut 
+
+class CartItemCreate(BaseModel):
+    product_id: int
+    quantity: int = 1
+
+
+class CartItemOut(BaseModel):
+    id: int
+    product_id: int
+    quantity: int
+    product: ProductOut
+
+    class Config:
+        orm_mode = True
+
+
+class CartOut(BaseModel):
+    id: int
+    items: List[CartItemOut]
+
+    class Config:
+        orm_mode = True
+
---
+++ app/schemas/order_schema.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+from .product_schema import ProductOut 
+
+class OrderItemOut(BaseModel):
+    id: int
+    product_id: int
+    quantity: int
+    product: ProductOut
+
+    class Config:
+        orm_mode = True
+
+class OrderOut(BaseModel):
+    id: int
+    created_at: datetime
+    items: List[OrderItemOut]
+
+    class Config:
+        orm_mode = True
